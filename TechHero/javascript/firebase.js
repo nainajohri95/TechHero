fed8b62e06eb1057cc9076408df549a7f350c31b
Naina Johri <93525285+nainajohri95@users.noreply.github.com>
2022-11-25 17:49:30 +0530

    new commits

---
+++ TechHero/javascript/firebase.js
@@ -0,0 +1,21 @@
+
+
+  
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFCoqlpTPtdNUvPfRgbfMWDgN-8OjE7JI",
+    authDomain: "fir-contactform-f50b7.firebaseapp.com",
+    projectId: "fir-contactform-f50b7",
+    storageBucket: "fir-contactform-f50b7.appspot.com",
+    messagingSenderId: "368380454033",
+    appId: "1:368380454033:web:7d7b0872a1e59766204b30"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
